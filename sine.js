a5b7c3636abd25b320ae7caaa52d53470f20b03b
unknown <admin@DESKTOP-OLTETAV.ads.autodesk.com>
2019-04-08 13:56:39 -0700

    minor changes

--- sine.js
+++ sine.js
@@ -3,10 +3,10 @@ H = 600;
 
 function setup() {
   createCanvas(W, H);
-  l1 = new Line(W/2, H/2, 100, PI, -0.1);
-  l2 = new Line(l1.x2, l1.y2, 45, PI/2, 0.4);
-  l3 = new Line(l2.x2, l1.y2, 15, PI/3, 0.33);
-  l4 = new Line(l3.x2, l3.y2, 85, PI/4, -0.25);
+  l1 = new Line(W/2, H/2, 120, PI, 0.35);
+  l2 = new Line(l1.x2, l1.y2, 35, PI/2, -0.25);
+  l3 = new Line(l2.x2, l1.y2, 25, PI/3, 0.33);
+  l4 = new Line(l3.x2, l3.y2, 55, PI/4, 0.5);
 }
 
 function draw() {
@@ -29,9 +29,8 @@ class Line {
     this.y1 = y;
     this.l = l;
     this.theta = theta;
-    this.x2 = this.l * Math.sin(this.theta) + this.x1;
-    this.y2 = this.l * Math.cos(this.theta) + this.y1;
     this.freq = freq;
+    this.update();
     this.history = [];
     this.color = [random(1) * 255, random(1) * 255, random(1) * 255];
   }
