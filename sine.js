197046ea4ee474886cf48f7ffb69880edee59dd8
unknown <admin@DESKTOP-OLTETAV.ads.autodesk.com>
2019-04-08 11:51:51 -0700

    added color

--- sine.js
+++ sine.js
@@ -3,10 +3,10 @@ H = 600;
 
 function setup() {
   createCanvas(W, H);
-  l1 = new Line(W/2, H/2, 100, PI, 0.1);
-  l2 = new Line(l1.x2, l1.y2, 75, PI/2, 0.25);
-  l3 = new Line(l2.x2, l1.y2, 35, PI/3, -0.33);
-  l4 = new Line(l3.x2, l3.y2, 25, PI/4, 0.5);
+  l1 = new Line(W/2, H/2, 100, PI, -0.1);
+  l2 = new Line(l1.x2, l1.y2, 45, PI/2, 0.4);
+  l3 = new Line(l2.x2, l1.y2, 15, PI/3, 0.33);
+  l4 = new Line(l3.x2, l3.y2, 85, PI/4, -0.25);
 }
 
 function draw() {
@@ -14,7 +14,7 @@ function draw() {
   l1.update();
   l1.draw();
   l2.update(l1);
-  l2.draw();
+  l2.draw(0);
   l3.update(l2);
   l3.draw();
   l4.update(l3);
@@ -33,6 +33,7 @@ class Line {
     this.y2 = this.l * Math.cos(this.theta) + this.y1;
     this.freq = freq;
     this.history = [];
+    this.color = [random(1) * 255, random(1) * 255, random(1) * 255];
   }
 
   update(parent) {
@@ -51,13 +52,13 @@ class Line {
       this.history.push(createVector(this.x2, this.y2));
     }
     noFill();
-    stroke(200,200,0);
+    stroke(this.color[0],this.color[1], this.color[2]);
     beginShape();
     for (let i = 0; i < this.history.length; i++) {
       vertex(this.history[i].x, this.history[i].y);
     }
     endShape();
-    stroke(0);
+    stroke(150);
     line(this.x1, this.y1, this.x2, this.y2);
   }
 }
